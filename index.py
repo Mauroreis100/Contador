46dbee4f6c6fda0bf447788fd6b61daa41562ae0
Mauro Reis <69197927+Mauroreis100@users.noreply.github.com>
2023-12-20 15:14:39 +0200

    Reading
    Reading works, missing saving from the entries. Changes to CSV files

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import json
 import os
 from objectivo import objectivo as item
 from tkinter import *
+import csv
 objectivosPath="M:\\PRJCTS\\Contador\\objectivos.txt"
 
 window=Tk()
@@ -14,13 +15,22 @@ if os.path.exists(objectivosPath):
 else:
     print("Path Problems")
 
-
+objectivos=[['Nome','Actual','Inicial','Final']]
+objectivos_nr=0
 try:
-    with open('objectivos.txt') as file:
-        label=Label(window,text="Os seus objectivos são: "+file.read())
+    with open('objectivos.csv', mode='r') as file:
+        csvFile = csv.reader(file)
+        i=0
+        label=Label(window,text="Os seus objectivos são: ")
         label.pack()
+        for lines in csvFile:  
+            labelObjectivo=Label(window,text=lines)
+            labelObjectivo.pack()
+            print(objectivos)
+            objectivos_nr=objectivos_nr+1
 except FileNotFoundError:
     print("Ficheiro não foi encontrado")
+# print(objectivos)
 def criar():
     windowCriar=Tk()
     windowCriar.geometry("420x420")
@@ -43,13 +53,29 @@ def criar():
     valor_inicial=entry_valorIni.get()
     valor_final=entry_valorFinal.get()
     pushUps=item(nome_item, valor_inicial, valor_final)
-    bt_Finalizar=Button(windowCriar,text='Finalizar',command=lambda: finalizar(pushUps))
+    bt_Finalizar=Button(windowCriar,text='Finalizar',command=lambda: finalizar(nome_item))
     bt_Finalizar.pack()
 
-def finalizar(pushUps):
-    with open('objectivos.txt','w') as file:
-        file.write(str(pushUps))
-        print("Novo objectivo adicionado",pushUps)
+# with open('objectivos.csv', 'w') as csvfile:  
+#     # creating a csv dict writer object  
+#     writer = csv.DictWriter(csvfile, fieldnames = ['Nome','Actual','Inicial','Final'])  
+        
+#     # writing headers (field names)  
+#     writer.writeheader()
+
+def finalizar(nome_item):
+    objectivos.append([nome_item])
+    print(objectivos)
+    
+    # with open('objectivos.csv','w') as file:
+    #     csvwriter = csv.writer(file)  
+        
+    #     # writing the fields  
+    #     csvwriter.writerow(objectivos[0])  
+        
+    #     for j in range(objectivos_nr):
+    #         if j!=0:
+    #             csvwriter.writerows(objectivos[j]) 
 bt_Criar=Button(window,text='Criar Objectivo', command=criar)
 bt_Criar.pack()
 window.mainloop()
